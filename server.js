828f22009e77931f6cbe4858f5e71d70d83f9689
[Ayman Shiham] <sharktank38000@gmail.com>
2023-10-17 20:21:02 -0400

    added a function to start app, added choices and added a switch statement to determine the selection

---
+++ server.js
@@ -0,0 +1,75 @@
+const mysql = require("mysql2");
+const inquirer = require("inquirer");
+
+// MYSQL connection
+const connection = mysql.createConnection({
+    host: "localhost",
+    port: 3333,
+    user: "root",
+    password: "",
+    database: "employeeTracker_db"
+})
+
+// start sql employee tracker
+function init(){
+    inquirer.createPromptModule({
+        type: "list",
+        name: "option",
+        message: "What would you want to do?",
+        choices: [
+            "View all Departments",
+            "View all Roles",
+            "View all Employees",
+            "Add a Department",
+            "Add a Role",
+            "Add a Employee",
+            "Add a Manager",
+            "Update an Employee Role",
+            "View Employees by Department",
+            "View Employees by Manager",
+            "Delete Departments | Roles | Employees",
+            "Exit"
+        ]
+    }).then((selected) => {
+        switch(selected.option){
+            case "View all Departments":
+                viewAllDepartments();
+                break;
+            case "View all Roles":
+                viewAllRoles();
+                break;
+            case "View all Employees":
+                viewAllEmployees();
+                break;
+            case "Add a Department":
+                addDepartment();
+                break;
+            case "Add a Role":
+                addRole();
+                break;
+            case "Add an Employee":
+                addEmployee();
+                break;
+            case "Add a Manager":
+                addManager();
+                break;
+            case "Update an Employee Role":
+                updateEmployeeRole();
+                break;
+            case "View Employees by Department":
+                viewEmployeeByDepartment();
+                break;
+            case "View Employees by Manager":
+                viewEmployeeByManager();
+                break;
+            case "Delete Departments | Roles | Employees":
+                deleteDRE();
+                break;
+            case "Exit":
+                connection.end();
+                console.log("Thank you GoodBye!");
+                break; 
+        }
+    })
+
+}
